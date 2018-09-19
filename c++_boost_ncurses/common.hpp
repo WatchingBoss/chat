74a5d99fbaa728e897b7fbedc9b353369d9e2917
WatchingBoss <opil94@yandex.ru>
2018-09-19 16:36:04 +0300

    externs for common.cpp

--- c++_boost_ncurses/common.hpp
+++ c++_boost_ncurses/common.hpp
@@ -8,4 +8,14 @@ using namespace boost::asio::ip;
 #define PORT_NUM 8888
 #define PORT_NUM_STR "8888"
 
+extern void sys_er(const char *);
+extern void sys_er(boost::system::error_code);
+
+#define EXCEPT_HANDLE(x)						\
+	try{x}										\
+	catch(std::exception &e){					\
+		std::cout << e.what() << std::endl;		\
+		sys_er(er_code);						\
+	}
+
 #endif

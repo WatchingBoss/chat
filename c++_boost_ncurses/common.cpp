adc67f712f68c6f9cb8e07048094b504ef2f7ad8
WatchingBoss <opil94@yandex.ru>
2018-09-19 16:34:44 +0300

    Create common cpp with sys_er funcs and except_handle macro

---
+++ c++_boost_ncurses/common.cpp
@@ -0,0 +1,17 @@
+#include "common.hpp"
+
+#include <iostream>
+
+void
+sys_er(const char *er)
+{
+	std::cout << er << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void
+sys_er(boost::system::error_code e_c)
+{
+	std::cout << "Code: " << e_c << "=> " << e_c.message() << std::endl;
+	exit(EXIT_FAILURE);
+}
